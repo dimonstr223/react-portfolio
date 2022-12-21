1bc579b80c9ce27b2ea18a0ecd7636ec342a3d89
dimonstr223 <dimstukmikolu@gmail.com>
2022-12-21 08:10:01 +0300

    Info component created and styled

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css'
+import './scss/App.scss'
+
+import Info from './components/Info'
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<div className='container'>
+				<Info />
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/Info.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import avatar from '../assets/img/me.jpg'
+import telegram from '../assets/img/icons/telegram.svg'
+import phone from '../assets/img/icons/phone.svg'
+import email from '../assets/img/icons/email.svg'
+
+const Info = () => {
+	return (
+		<div className='info'>
+			<div className='info__top'>
+				<div className='avatar__wrapper'>
+					<img src={avatar} alt='Avatar' />{' '}
+				</div>
+				<h1 className='name'>Dmitry Mikolutsky</h1>
+				<div>
+					<button className='about__button'>About me</button>
+				</div>
+			</div>
+			<div className='info__bottom'>
+				<h2 className='title'>Frontend - developer</h2>
+				<div className='contacts'>
+					<ul className='contacts__list'>
+						<li className='contacts__item'>
+							<img src={telegram} width={30} alt='Telegram' />
+						</li>
+						<li className='contacts__item'>
+							<img src={phone} width={30} alt='Phone' />
+						</li>
+						<li className='contacts__item'>
+							<img src={email} width={30} alt='Email' />
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Info
