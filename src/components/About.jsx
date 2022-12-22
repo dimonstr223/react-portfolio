3a0b8a819928b957c566bf78e93558be2490ab30
dimonstr223 <dimstukmikolu@gmail.com>
2022-12-22 11:50:16 +0300

    About component created

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+	return (
+		<div className='about'>
+			<p className='about__text'>
+				I develop both static web-pages and dynamic web-applications. I am an
+				independent and teacheble person. I constantly learn something new and
+				enjoy my progress.
+			</p>
+		</div>
+	)
+}
+
+export default About
