e8c284d749ccbec68d61470ad5dec281755abe87
dimonstr223 <dimstukmikolu@gmail.com>
2022-12-23 09:02:59 +0300

    Socials links added. Info module scss added

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,43 +6,62 @@ import phone from '../assets/img/icons/phone.svg'
 import email from '../assets/img/icons/email.svg'
 import About from './About'
 
+import styles from '../scss/components/Info.module.scss'
+
 const Info = () => {
 	const [isAboutOpened, setIsAboutOpened] = React.useState(false)
 	console.log(isAboutOpened)
 
 	return (
-		<div className='info'>
-			<div className='info__top'>
-				<div className='avatar__wrapper'>
+		<div className={styles.info}>
+			<div className={styles.info__top}>
+				<div className={styles.avatar__wrapper}>
 					<img src={avatar} alt='Avatar' />
 				</div>
-				<h1 className='name'>Dmitry Mikolutsky</h1>
+				<h1 className={styles.name}>Dmitry Mikolutsky</h1>
 				<div
-					className='about__wrapper'
+					className={styles.about__wrapper}
 					onMouseEnter={() => setIsAboutOpened(true)}
 					onMouseLeave={() => setIsAboutOpened(false)}
 					style={{ position: 'relative' }}
 				>
 					<button
-						className={isAboutOpened ? 'about__button_active' : 'about__button'}
+						className={
+							isAboutOpened
+								? styles['about__button_active']
+								: styles['about__button']
+						}
 					>
 						About me
 					</button>
 					<About isAboutOpened={isAboutOpened} />
 				</div>
 			</div>
-			<div className='info__bottom'>
-				<h2 className='title'>Frontend - developer</h2>
-				<div className='contacts'>
-					<ul className='contacts__list'>
-						<li className='contacts__item'>
-							<img src={telegram} width={30} alt='Telegram' />
+			<div className={styles.info__bottom}>
+				<h2 className={styles.title}>Frontend - developer</h2>
+				<div className={styles.contacts}>
+					<ul className={styles.contacts__list}>
+						<li className={styles.contacts__item}>
+							<a
+								href='https://t.me/sqoothq'
+								target='_blank'
+								className={styles.contacts__link}
+							>
+								<img src={telegram} width={30} alt='Telegram' />
+							</a>
 						</li>
-						<li className='contacts__item'>
-							<img src={phone} width={30} alt='Phone' />
+						<li className={styles.contacts__item}>
+							<a href='tel:375295669251' className={styles.contacts__link}>
+								<img src={phone} width={30} alt='Phone' />
+							</a>
 						</li>
-						<li className='contacts__item'>
-							<img src={email} width={30} alt='Email' />
+						<li className={styles.contacts__item}>
+							<a
+								href='mailto:dimstukmikolu@gmail.com'
+								className={styles.contacts__link}
+							>
+								<img src={email} width={30} alt='Email' />
+							</a>
 						</li>
 					</ul>
 				</div>
